ef6db6d582665bb586e1824837d7cbda467c0301
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-02-07 16:59:11 +0100

    Better Card Layout UI in Trainings Section

--- src/components/Pages/Training/Times.tsx
+++ src/components/Pages/Training/Times.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import MyTitle from "../../Utils/MyTitle";
 import {times} from "../../Content/TrainerContent";
-import {Card, Group, Badge, Container, Space, Text} from "@mantine/core";
+import {Card, Group, Stack, Grid, Badge, Container, Space, Text} from "@mantine/core";
 import React from "react";
 
 function CourseCard(props: any) {
@@ -13,15 +13,18 @@ function CourseCard(props: any) {
 
                 <Group position="apart" mt="md" mb="xs">
                     <Text weight={500}>{props.title}</Text>
-                    <Group position="apart" mt="md" mb="xs">
-                        <Badge color={props.color} variant="outline"> {props.day} </Badge>
-                        <Badge color={props.color} variant="outline"> {props.time} </Badge>
-                    </Group>
                 </Group>
 
                 <Text ta="left" size="sm" color="dimmed">
                     {props.desc}
                 </Text>
+
+                <Space h="md" />
+
+                <Group>
+                    <Badge color={props.color} variant="outline"> {props.day} </Badge>
+                    <Badge color={props.color} variant="outline"> {props.time} </Badge>
+                </Group>
             </Card>
             <Space h="xl" />
         </>
@@ -35,34 +38,48 @@ export default function Times() {
             <MyTitle content={times[current_lang][0]} />
             <Space h="xl" />
             {/*<MyContent content={times[current_lang][1]} />*/}
-            <CourseCard
-                title={times[current_lang][2]}
-                desc={times[current_lang][3]}
-                day={times[current_lang][4]}
-                time={"18:00 - 19:00"}
-                color={"teal"}
-            />
-            <CourseCard
-                title={times[current_lang][5]}
-                desc={times[current_lang][6]}
-                day={times[current_lang][4]}
-                time={"19:00 - 21:00"}
-                color={"orange"}
-            />
-            <CourseCard
-                title={times[current_lang][7]}
-                desc={times[current_lang][9]}
-                day={times[current_lang][8]}
-                time={"18:30 - 20:30"}
-                color={"dark"}
-            />
-            <CourseCard
-                title={times[current_lang][10]}
-                desc={times[current_lang][12]}
-                day={times[current_lang][11]}
-                time={"08:00 - 12:00"}
-                color={"grape"}
-            />
+            <Stack>
+                <Grid>
+                    <Grid.Col md={6}>
+                        <CourseCard
+                            title={times[current_lang][2]}
+                            desc={times[current_lang][3]}
+                            day={times[current_lang][4]}
+                            time={"18:00 - 19:00"}
+                            color={"teal"}
+                        />
+                    </Grid.Col>
+                    <Grid.Col md={6}>
+                        <CourseCard
+                            title={times[current_lang][5]}
+                            desc={times[current_lang][6]}
+                            day={times[current_lang][4]}
+                            time={"19:00 - 21:00"}
+                            color={"orange"}
+                        />
+                    </Grid.Col>
+                </Grid>
+            </Stack>
+                <Grid>
+                    <Grid.Col md={6}>
+                        <CourseCard
+                            title={times[current_lang][7]}
+                            desc={times[current_lang][9]}
+                            day={times[current_lang][8]}
+                            time={"18:30 - 20:30"}
+                            color={"dark"}
+                        />
+                    </Grid.Col>
+                    <Grid.Col md={6}>
+                        <CourseCard
+                            title={times[current_lang][10]}
+                            desc={times[current_lang][12]}
+                            day={times[current_lang][11]}
+                            time={"08:00 - 12:00"}
+                            color={"grape"}
+                        />
+                    </Grid.Col>
+                </Grid>
             <CourseCard
                 title={times[current_lang][13]}
                 desc={times[current_lang][14]}
